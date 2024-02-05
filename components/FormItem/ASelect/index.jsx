0e91b70f7f6d379e3b57f05712ece96110d633d2
xuhonghui <15859256852@163.com>
2024-02-05 18:38:08 +0800

    优化 input,select 组件

--- components/FormItem/ASelect/index.jsx
+++ components/FormItem/ASelect/index.jsx
@@ -3,10 +3,23 @@ import { Form, Select } from 'antd';
 import Style from './ASelect.module.scss';
 
 const ASelect = (props) => {
-  const { label, model, rules,options, defaultValue, placeholder, onChange, showSearch = true, autoClearSearchValue = true, allowClear = true, disabled = false, popupClassName } = props;
+  const { label, model, rules, options, defaultValue, placeholder, onChange, showSearch = true, autoClearSearchValue = true, allowClear = true, disabled = false, popupClassName } = props;
   return (
-    <Form.Item label={label} name={model} rules={rules}>
-      <Select
+    (label || label === ' ') ?
+      (<Form.Item label={label} name={model} rules={rules}>
+        <Select
+          defaultValue={defaultValue}
+          placeholder={placeholder}
+          showSearch={showSearch}
+          autoClearSearchValue={autoClearSearchValue}
+          onChange={onChange}
+          options={options}
+          allowClear={allowClear}
+          disabled={disabled}
+          popupClassName={popupClassName}
+        />
+      </Form.Item>) :
+      (<Select
         defaultValue={defaultValue}
         placeholder={placeholder}
         showSearch={showSearch}
@@ -16,8 +29,7 @@ const ASelect = (props) => {
         allowClear={allowClear}
         disabled={disabled}
         popupClassName={popupClassName}
-      />
-    </Form.Item>
+      />)
   );
 };
 
